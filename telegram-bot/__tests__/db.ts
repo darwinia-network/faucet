a9dfcc2dca9b8a32d80f6d2b82809090a676b9e4
fewensa <fewensa@protonmail.com>
2021-09-15 16:29:40 +0800

    clean

--- telegram-bot/__tests__/db.ts
+++ telegram-bot/__tests__/db.ts
@@ -1,4 +1,6 @@
 
+// todo: migrate https://github.com/darwinia-network/faucet/blob/d52a7becc83e1c6d60c5072b6ce4d1d60372dac5/telegram-bot/__tests__/dactle.ts
+
 test('hello', async () => {
   return new Promise(resolve => resolve(true));
 });
